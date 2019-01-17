48187c38d6420590c081de9a13b37f36ff14a8d2
ByDenis <dstetskiy@live.com>
2019-01-17 17:03:45 +0300

    add UWP Theme

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//export * from './app.js';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+
+import './styles/css.css';
+
+ReactDOM.render(<App />, document.getElementById("root"));
